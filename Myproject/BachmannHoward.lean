024a78640d06c5dc6bdd952d5fbe70ce7237f36c
shoshark0525@gmail.com <shoshark0525@gmail.com>
2026-08-06 00:19:20 -0400

    BH-ordinal update with new notation/def/comparison/normality

--- Myproject/BachmannHoward.lean
+++ Myproject/BachmannHoward.lean
@@ -4,17 +4,15 @@ import Mathlib
 -- Definition
 --===============================================================================
 /-
-Unlike the ordinals < ε₀ and < Γ₀, uncountable ordinals with the Ω-form "collapses"
-into a countable form represented with the Veblen hierarchy. So, we define two
-separate, although mutual, cases of (i) countable ordinals and (ii) uncountable
-ordinals (technically, Ω-form term).
+We follow Fernandez-Duque and Weiermann's notations. We represent the parameterized
+collapsing function θ_X(ζ) as ψ(ζ) := θ_P(ζ) where P = {ω^{α} : α ∈ On}, the class
+of principal ordinals (this does not lose any generality).
 -/
 mutual
 inductive countableOrd where
-  | sum : List cPrincipal → countableOrd
-inductive cPrincipal where
-  | phi : countableOrd → countableOrd → cPrincipal
-  | psi : omegaTerm → cPrincipal
+  | sum : List principal → countableOrd
+inductive principal where
+  | psi : omegaTerm → principal
 -- ζ = Ω^{α}β + γ
 inductive omegaTerm where
   | zero : omegaTerm
@@ -23,115 +21,211 @@ inductive omegaTerm where
 end
 
 
-
-
-
 --===============================================================================
 -- Preliminaries
 --===============================================================================
 namespace countableOrd
 def zero : countableOrd := sum []
-def ofPrincipal (p : cPrincipal) := sum [p] -- principal ordinal in terms of sum
-def one : countableOrd := ofPrincipal (cPrincipal.phi zero zero)
-def two : countableOrd := sum [(.phi zero zero), (.phi zero zero)]
-def omega : countableOrd := ofPrincipal (.phi zero one)
-def epsilonZero : countableOrd := ofPrincipal (.phi one zero)
+def ofPrincipal (p : principal) : countableOrd := sum [p]
 end countableOrd
 
-/-
-As seen of the redunducy of the notation for zero (countableOrd.sum [] and omegaTerm.zero),
-we embed countable ordinal notations into the omega term. Mathematically, any
-countable ordinal β < Ω can be represented as Ω^{0}β + 0.
--/
-namespace omegaTerm
-def ofCountable : countableOrd → omegaTerm
-| countableOrd.sum [] => omegaTerm.zero
-| countableOrd.sum (p :: ps) => omegaTerm.omegaNF omegaTerm.zero
-                                (countableOrd.sum (p :: ps)) omegaTerm.zero
-def one : omegaTerm := ofCountable (countableOrd.one)
-def two : omegaTerm := ofCountable (countableOrd.two)
-def bigOmega : omegaTerm := omegaNF one countableOrd.zero zero
+namespace principal
+def one : principal := psi omegaTerm.zero
+end principal
 
-/-
-As in Fernandez-Duque/Weiermann's paper, we define the function C(ζ) to retrieve
-the coefficients of the Ω-form. Specifically, if ζ = Ω^{α}β + γ,
-C(ζ) = C(α) ∪ C(γ) ∪ {β}
--/
+namespace countableOrd
+def one : countableOrd := ofPrincipal principal.one
+def two : countableOrd := sum [principal.one, principal.one]
+end countableOrd
+
+namespace omegaTerm
+-- Every countable ordinal β < Ω can be represented as Ω^{0}β+0
+def ofCountable : countableOrd → omegaTerm
+  | .sum [] => zero
+  | .sum (p :: ps) => omegaNF zero (.sum (p :: ps)) zero
+def one : omegaTerm := ofCountable (countableOrd.one)
+def bigOmega : omegaTerm := omegaNF one countableOrd.one zero
 def coefficients : omegaTerm → List countableOrd
   | zero => [countableOrd.zero]
   | omegaNF alpha beta gamma => (coefficients alpha) ++ (coefficients gamma) ++ [beta]
 end omegaTerm
 
+namespace countableOrd
+def omega : countableOrd := ofPrincipal (.psi omegaTerm.one) -- lemma 6.8
+end countableOrd
 
 
 --===============================================================================
--- Comparison / Order
+-- Comparison
 --===============================================================================
 /-
-The comparison between countables and φ_{α}(β) are simple. Pohler provides the following:
-        φ_{α}(β) < φ_{γ}(δ) iff (i) α < γ and β < φ_{γ}(δ),
-                                (ii) α = γ and β < δ
-                                (iii) γ < α and φ_{α}(β) < δ
-However, when we consider the ψ function, it becomes very complicated. We follow Fernandez-Duque/
-Weiermann's notations for the explanation that follows. Given a parameterized collapsing function
-Θ_X : ε_{Ω+1} → Ω, they introduce lemma 3.3.,
-        If α < β < ε_{Ω+1}, then Θ_X(α) < Θ_X(β) iff α^* < Θ_X(β).
-The collapsing function Θ_X(ζ) is defined to be the least θ ∈ X such that ζ^* < θ < Ω and for all
-ζ < ξ, if ζ^* < θ then θ_X(ζ) < θ. ChatGPT puts this in very simple words: "θ_X(ζ)is the smallest
-allowed countable ordinal above every coefficient of ξ, which is also closed above the collapses
-of all earlier arguments whose coefficients fit below it." In this formalization ψ ≃ θ_X.
-        ψ(α) < ψ (β) iff (i) α < β and α^* < ψ(β), or
-                         (ii) β < α and ψ(α) ≤ β^*
-
+The comparison of two ψ functions is given in lemma 3.3. That is,
+  (i) given α < β < ε_{Ω+1}, ψ(α) < ψ(β) iff α^* < ψ(β)
+  (ii) given β < α < ε_{Ω+1}, ψ(α) < ψ(β) iff ψ(α) ≤ β^*
+to represent these two statements, we consider the follwing equivalent statements:
+  (i*) α^* < ψ(β) iff ∀ c ∈ C(α), c < ψ(β)
+  (ii*)  ψ(α) ≤ β^* iff ∃ c ∈ C(β), ψ(α) < c or ψ(α) = c
 -/
-
 mutual
 inductive countableOrd_lt : countableOrd → countableOrd → Prop where
-  | sum {xs ys : List cPrincipal} (h : cPrincipalList_lt xs ys) :
+  | sum {xs ys : List principal} (h : principalList_lt xs ys) :
         countableOrd_lt (countableOrd.sum xs) (countableOrd.sum ys)
 inductive countableOrd_eq : countableOrd → countableOrd → Prop where
-  | sum {xs ys : List cPrincipal} (h : cPrincipalList_eq xs ys) :
+  | sum {xs ys : List principal} (h : principalList_eq xs ys) :
         countableOrd_eq (countableOrd.sum xs) (countableOrd.sum ys)
-inductive cPrincipal_lt : cPrincipal → cPrincipal → Prop where
-  -- The cases excluding ψ
-  | phi_left {a b c d : countableOrd} (h1 : countableOrd_lt a c)
-              (h2 : countableOrd_lt b (countableOrd.ofPrincipal (cPrincipal.phi c d))) :
-              cPrincipal_lt (cPrincipal.phi a b) (cPrincipal.phi c d)
-  | phi_eq {a b c d : countableOrd} (h1 : countableOrd_eq a c) (h2 : countableOrd_lt b d) :
-              cPrincipal_lt (cPrincipal.phi a b) (cPrincipal.phi c d)
-  | phi_right {a b c d : countableOrd} (h1 : countableOrd_lt c a)
-              (h2 : countableOrd_lt (countableOrd.ofPrincipal (cPrincipal.phi a b)) d) :
-              cPrincipal_lt (cPrincipal.phi a b) (cPrincipal.phi c d)
-  -- The cases of comparing ψ(α) and ψ(β)
+inductive principal_lt : principal → principal → Prop where
   | psi_forward {a b : omegaTerm} (h1 : omegaTerm_lt a b)
-        (h2 : ∀c ∈ (omegaTerm.coefficients a), countableOrd_lt c (countableOrd.ofPrincipal (cPrincipal.psi b))) :
-          cPrincipal_lt (cPrincipal.psi a) (cPrincipal.psi b)
-  | psi_reverse {a b : omegaTerm} (h1 : omegaTerm_lt b a)
-        (h2 : ∃ c ∈ omegaTerm.coefficients b,
-        countableOrd_lt (countableOrd.ofPrincipal (cPrincipal.psi a)) c ∨
-        countableOrd_eq (countableOrd.ofPrincipal (cPrincipal.psi a)) c) :
-          cPrincipal_lt (cPrincipal.psi a) (cPrincipal.psi b)
-  -- The cases of comparing φ_{α}(β) and ψ(γ)
-inductive cPrincipal_eq : cPrincipal → cPrincipal → Prop where
-  | phi {a b c d : countableOrd} (hac : countableOrd_eq a c) (hbd : countableOrd_eq b d) :
-        cPrincipal_eq (cPrincipal.phi a b) (cPrincipal.phi c d)
-  | psi {a b : omegaTerm} (hab : omegaTerm_eq a b) :
-        cPrincipal_eq (cPrincipal.psi a) (cPrincipal.psi b)
-inductive cPrincipalList_lt : List cPrincipal → List cPrincipal → Prop where
-  | nil_cons {p : cPrincipal} {ps : List cPrincipal} : cPrincipalList_lt [] (p :: ps)
-  | head {p q : cPrincipal} {ps qs : List cPrincipal} (h : cPrincipal_lt p q) :
-          cPrincipalList_lt (p :: ps) (q :: qs)
-  | tail {p q : cPrincipal} {ps qs : List cPrincipal} (h1 : cPrincipal_eq p q)
-         (h2 : cPrincipalList_lt ps qs) : cPrincipalList_lt (p :: ps) (q :: qs)
-inductive cPrincipalList_eq : List cPrincipal → List cPrincipal → Prop where
-  | nil : cPrincipalList_eq [] []
-  | cons {x y : cPrincipal} {xs ys : List cPrincipal} (h1 : cPrincipal_eq x y)
-         (h2 : cPrincipalList_eq xs ys) : cPrincipalList_eq (x :: xs) (y :: ys)
+      (h2 : coefficientList_lt (omegaTerm.coefficients a) (countableOrd.ofPrincipal (.psi b))) :
+                principal_lt (.psi a) (.psi b)
+  | psi_reverse_lt {a b : omegaTerm} {c : countableOrd} (h1 : omegaTerm_lt b a)
+                   (h2 : c ∈ omegaTerm.coefficients b)
+                   (h3 : countableOrd_lt (countableOrd.ofPrincipal (.psi a)) c) :
+                  principal_lt (.psi a) (.psi b)
+  | psi_reverse_eq {a b : omegaTerm} {c : countableOrd} (h1 : omegaTerm_lt b a)
+                   (h2 : c ∈ omegaTerm.coefficients b)
+                   (h3 : countableOrd_eq (countableOrd.ofPrincipal (.psi a)) c) :
+                  principal_lt (.psi a) (.psi b)
+inductive principal_eq : principal → principal → Prop where
+  | psi {a b : omegaTerm} (h : omegaTerm_eq a b) : principal_eq (.psi a) (.psi b)
+inductive principalList_lt : List principal → List principal → Prop where
+  | nil {p : principal} {ps : List principal} : principalList_lt [] (p :: ps)
+  | head {p q : principal} {ps qs : List principal} (h : principal_lt p q) :
+         principalList_lt (p :: ps) (q :: qs)
+  | tail {p q : principal} {ps qs : List principal} (h1 : principal_eq p q)
+         (h2 : principalList_lt ps qs) : principalList_lt (p :: ps) (q :: qs)
+inductive principalList_eq : List principal → List principal → Prop where
+  | nil : principalList_eq [] []
+  | cons {p q : principal} {ps qs : List principal} (h1 : principal_eq p q)
+         (h2 : principalList_eq ps qs) : principalList_eq (p :: ps) (q :: qs)
 inductive omegaTerm_lt : omegaTerm → omegaTerm → Prop where
+  | zero {alpha gamma : omegaTerm} {beta : countableOrd} :
+         omegaTerm_lt omegaTerm.zero (omegaTerm.omegaNF alpha beta gamma)
+  | exponent {alpha1 alpha2 gamma1 gamma2 : omegaTerm} {beta1 beta2 : countableOrd}
+         (h : omegaTerm_lt alpha1 alpha2) :
+         omegaTerm_lt (omegaTerm.omegaNF alpha1 beta1 gamma1)
+                      (omegaTerm.omegaNF alpha2 beta2 gamma2)
+  | coefficient {alpha1 alpha2 gamma1 gamma2 : omegaTerm} {beta1 beta2 : countableOrd}
+         (h1 : omegaTerm_eq alpha1 alpha2) (h2 : countableOrd_lt beta1 beta2) :
+         omegaTerm_lt (omegaTerm.omegaNF alpha1 beta1 gamma1)
+                      (omegaTerm.omegaNF alpha2 beta2 gamma2)
+  | remainder {alpha1 alpha2 gamma1 gamma2 : omegaTerm} {beta1 beta2 : countableOrd}
+         (h1 : omegaTerm_eq alpha1 alpha2) (h2 : countableOrd_eq beta1 beta2)
+         (h3 : omegaTerm_lt gamma1 gamma2) :
+         omegaTerm_lt (omegaTerm.omegaNF alpha1 beta1 gamma1)
+                      (omegaTerm.omegaNF alpha2 beta2 gamma2)
 inductive omegaTerm_eq : omegaTerm → omegaTerm → Prop where
   | zero : omegaTerm_eq omegaTerm.zero omegaTerm.zero
-  -- Ω^{α}β+γ and Ω^{c}d+δ
-  | omegaNF {a c gamma delta : omegaTerm} {b d : countableOrd} (h1 : omegaTerm_eq a c)
-            (h2 : omegaTerm_eq gamma delta) (h3 : countableOrd_eq b d) :
-            omegaTerm_eq (omegaTerm.omegaNF a b gamma) (omegaTerm.omegaNF c d delta)
+  | omegaNF {alpha1 alpha2 gamma1 gamma2 : omegaTerm} {beta1 beta2 : countableOrd}
+            (h1 : omegaTerm_eq alpha1 alpha2) (h2 : countableOrd_eq beta1 beta2)
+            (h3 : omegaTerm_eq gamma1 gamma2) :
+            omegaTerm_eq (omegaTerm.omegaNF alpha1 beta1 gamma1)
+                         (omegaTerm.omegaNF alpha2 beta2 gamma2)
+inductive coefficientList_lt : List countableOrd → countableOrd → Prop where
+  | nil {bound : countableOrd} : coefficientList_lt [] bound
+  | cons {c bound : countableOrd} {cs : List countableOrd} (hhead : countableOrd_lt c bound)
+         (htail : coefficientList_lt cs bound) :
+         coefficientList_lt (c :: cs) bound
 end
+infix:50 "<c" => countableOrd_lt
+infix:50 "=c" => countableOrd_eq
+infix:50 "<p" => principal_lt
+infix:50 "=p" => principal_eq
+infix:50 "<o" => omegaTerm_lt
+infix:50 "=o" => omegaTerm_eq
+
+def countableOrd_le (a b : countableOrd) : Prop :=
+  countableOrd_lt a b ∨ countableOrd_eq a b
+def principal_le (a b : principal) : Prop :=
+  principal_lt a b ∨ principal_eq a b
+def principal_ge (a b : principal) : Prop :=
+  principal_lt b a ∨ principal_eq a b
+def omegaTerm_le (a b : omegaTerm) : Prop :=
+  omegaTerm_lt a b ∨ omegaTerm_eq a b
+infix:50 "≤c" => countableOrd_le
+infix:50 "≤p" => principal_le
+infix:50 "≥p" => principal_ge
+infix:50 "≤o" => omegaTerm_le
+
+
+--===============================================================================
+-- Normality
+--===============================================================================
+/-
+Just like the conditions for a valid Cantor Normal Form, we inspect the validity, or
+normality of a ψ-function here.
+-/
+mutual
+-- Normal if the list of principal sum is in decreasing normal form
+inductive countableOrd_normal : countableOrd → Prop where
+  | sum {ps : List principal} (h : principalList_normal ps) : countableOrd_normal (.sum ps)
+-- Normal if the argument α of ψ(α) is normal
+inductive principal_normal : principal → Prop where
+  | psi {a : omegaTerm} (h : omegaTerm_normal a) : principal_normal (.psi a)
+-- Normal if each principal is normal and the list is in nonincreasing order
+inductive principalList_normal : List principal → Prop where
+  | nil : principalList_normal []
+  | singleton {p : principal} (h : principal_normal p) : principalList_normal [p]
+  | cons {p q : principal} {qs : List principal} (h1 : principal_normal p)
+         (h2 : principalList_normal (q :: qs)) (h3 : p ≥p q) :
+         principalList_normal (p :: q :: qs)
+/-
+Ω^{α}β + γ is normal if
+  (i) α is normal
+  (ii) β is normal positive countable (0 < β < Ω)
+  (iii) γ is normal
+  (iv) γ < Ω^{α}
+-/
+inductive omegaTerm_normal : omegaTerm → Prop where
+  | zero : omegaTerm_normal .zero
+  | omegaNF {alpha gamma : omegaTerm} {beta : countableOrd}
+            (h1 : omegaTerm_normal alpha)
+            (h2 : countableOrd_normal beta)
+            (h3 : omegaTerm_normal gamma)
+            (h4 : countableOrd.zero <c beta)
+            (h5 : gamma <o .omegaNF alpha (countableOrd.one) (omegaTerm.zero)) :
+      omegaTerm_normal (.omegaNF alpha beta gamma)
+end
+
+
+--===============================================================================
+-- Some verification
+
+theorem countableOrd_zero_lt_one :
+    countableOrd.zero <c countableOrd.one := by
+  simpa [countableOrd.zero, countableOrd.one, countableOrd.ofPrincipal]
+  using countableOrd_lt.sum (principalList_lt.nil (p := principal.one) (ps := []))
+theorem countableOrd_zero_normal :
+    countableOrd_normal countableOrd.zero := by
+  exact countableOrd_normal.sum principalList_normal.nil
+theorem principal_one_normal : principal_normal principal.one := by
+  exact principal_normal.psi omegaTerm_normal.zero
+theorem countableOrd_one_normal : countableOrd_normal countableOrd.one := by
+  exact countableOrd_normal.sum (principalList_normal.singleton principal_one_normal)
+theorem omegaTerm_one_normal : omegaTerm_normal omegaTerm.one := by
+  change omegaTerm_normal (.omegaNF
+      omegaTerm.zero
+      countableOrd.one
+      omegaTerm.zero)
+  apply omegaTerm_normal.omegaNF
+  · exact omegaTerm_normal.zero
+  · exact countableOrd_one_normal
+  · exact omegaTerm_normal.zero
+  · exact countableOrd_zero_lt_one
+  · exact omegaTerm_lt.zero
+theorem bigOmega_normal :
+    omegaTerm_normal omegaTerm.bigOmega := by
+  change omegaTerm_normal
+    (.omegaNF
+      omegaTerm.one
+      countableOrd.one
+      omegaTerm.zero)
+  apply omegaTerm_normal.omegaNF
+  · exact omegaTerm_one_normal
+  · exact countableOrd_one_normal
+  · exact omegaTerm_normal.zero
+  · exact countableOrd_zero_lt_one
+  · exact omegaTerm_lt.zero
+
+--===============================================================================
+-- Reflexivity of =
+--===============================================================================
